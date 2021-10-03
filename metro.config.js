441bbff2c77a96aceec8100e37df24dbe49d95f6
Jalada <126989+jalada@users.noreply.github.com>
2021-10-03 17:18:50 +0100

    Upgrade react-native

--- metro.config.js
+++ metro.config.js
@@ -24,7 +24,7 @@ module.exports = {
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
-        inlineRequires: false,
+        inlineRequires: true,
       },
     }),
   },
